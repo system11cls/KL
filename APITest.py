becd72fc331eb4f449fa4ac917d1fb44d93026f6
system1cls <d.faranosov@gmail.com>
2026-02-17 13:29:12 +0700

    fix

--- APITest.py
+++ APITest.py
@@ -1,6 +1,6 @@
 from DBapi import CipherApi, CipherTools
 
-driver = CipherApi("neo4j://127.0.0.1:7687", "neo4j", "Dima08112004")
+driver = CipherApi("neo4j://127.0.0.1:7687", "neo4j", "neo4j")
 
 print(CipherTools.generate_random_string())
 
